6f4715c9854379ee563a64d279ca14e1da640a9f
Tyler Spadgenske <tyspadgens@gmail.com>
2015-02-25 19:04:39 +0000

    Got serial port and python working

--- src/serialport.py
+++ src/serialport.py
@@ -6,28 +6,29 @@ import serial, time
 
 class SerialPort():
     def __init__(self):
-       pass
-    
+       self.baud_rate = 115200
+       
     def connect(self):
         #open connection
-        self.serialport = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
+        self.serialport = serial.Serial("/dev/ttyAMA0", self.baud_rate, timeout=0.5)
         #Send test command
-        self.serialport.write('\nAT')
+        self.serialport.write('AT\r')
         #get reply 
-        reply = self.serialport.readlines()
+        reply = self.serialport.read(50).rstrip()
         print(reply)
         #Check for good return
-        try:
-            if reply[1][0] == 'O':
-                print 'Ćonnected to FONA'
-            else:
-                print 'Error Comunicating with FONA'
-        except:
-            print 'Error comunicating with FONA'
+        if 'OK' in reply:
+            print 'Connected to FONA'
+        else:
+            print 'Error Comunicating with FONA'
+            self.serialport.close()
+
     def transmit(self, data):
-        self.serialport.write('\n' + data)
-        self.serialport.readlines()
-        print self.serialport.readlines()
+        self.serialport.write(data + '\r')
+        feed = self.serialport.readlines()
+        for i in range(len(feed)):
+            feed[i] = feed[i].rstrip()
+        return feed
     
     def check(self):
         self.model = self.transmit('ATI')
@@ -46,3 +47,4 @@ if __name__ == '__main__':
     test = SerialPort()
     test.connect()
     test.check()
+
